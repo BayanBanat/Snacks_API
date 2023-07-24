5754dbdb7fc4dfdcca0121638c39126948a92334
BayanBanat <bayankbanat99@gmail.com>
2023-07-24 08:34:30 +0000

    Snacks_API

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+
+# Lab: Class 31
 # Snacks_API
+**Author: Bayan**
+
+**Setup**
+python -m venv .venv      
+
+source .venv/bin/activate 
+
+docker-compose up          
+
+
+**PORT** - 8000
+
+
+How to initialize/run your application:  python manage.py runserver 
---
+++ snacks/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Snack
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from .serializers import SnackSerializer
+# Create your views here.
+
+class SnackListView(ListCreateAPIView):
+
+    queryset = Snack.objects.all()
+    serializer_class = SnackSerializer
+
+
+class SnackDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Snack.objects.all()
+    serializer_class = SnackSerializer
+
+
